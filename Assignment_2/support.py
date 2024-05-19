2eb27addda0ac674d4e80b2ae924c7b091214e40
LucidCodeAI <lutchocode@gmail.com>
2024-05-19 09:36:44 +0200

    Update model with validation

--- Assignment_2/support.py
+++ Assignment_2/support.py
@@ -1,13 +1,18 @@
 from torchvision import datasets, transforms
+import torch
 
 
 def load_dataset():
+    
+    mean = [0.3699, 0.2424, 0.2564]
+    std = [0.0888, 0.1932, 0.1779]
 
     transform = transforms.Compose(
         [
             transforms.Resize([105, 78]),
             transforms.CenterCrop(size=[60, 30]),
             transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
         ]
     )
 
@@ -23,6 +28,22 @@ def load_dataset():
 
     return train_dataset, test_dataset
 
+def calculate_mean_std(dataset):
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=2)
 
-# train_dataset, test_dataset = load_dataset()
-# print(len(train_dataset), len(test_dataset))
+    mean = 0.0
+    for images, _ in dataloader:
+        batch_samples = images.size(0) 
+        images = images.view(batch_samples, images.size(1), -1)
+        mean += images.mean(2).sum(0)
+    mean = mean / len(dataloader.dataset)
+
+    var = 0.0
+    for images, _ in dataloader:
+        batch_samples = images.size(0)
+        images = images.view(batch_samples, images.size(1), -1)
+        var += ((images - mean.unsqueeze(1))**2).sum([0,2])
+    std = torch.sqrt(var / (len(dataloader.dataset)*images.size(2)))
+
+    return mean, std
+
