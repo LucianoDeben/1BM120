0058ac49a6b8df0f9b62734a44b7851b759b1369
LucidCodeAI <lutchocode@gmail.com>
2024-05-29 11:21:18 +0200

    Add Notebook clean up

--- Assignment_2/model.py
+++ Assignment_2/model.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+        
 class SimpleCNN(nn.Module):
     """
     Simple CNN model for binary classification
--- Assignment_2/train_test_optimize.py
+++ Assignment_2/train_test_optimize.py
@@ -3,10 +3,10 @@ from torch.utils.data import DataLoader, random_split, Subset
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import KFold
 
-def train_model(model, dataset, criterion, optimizer, device, n_epochs=10, val_split=0.1, model_name="simple_model"):
+def train_model(model, train_data, criterion, optimizer, device, n_epochs=10, val_split=0.1, model_name="simple_model"):
     """
     Train a PyTorch model on a dataset
     
@@ -24,9 +24,9 @@ def train_model(model, dataset, criterion, optimizer, device, n_epochs=10, val_s
     - train_losses: list of training losses
     - val_losses: list of validation losses
     """
-    val_size = int(val_split * len(dataset))
-    train_size = len(dataset) - val_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    train_size = int((1 - val_split) * len(train_data))
+    val_size = len(train_data) - train_size
+    train_dataset, val_dataset = random_split(train_data, [train_size, val_size])
 
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, drop_last=False)
     val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True, drop_last=False)
@@ -39,7 +39,8 @@ def train_model(model, dataset, criterion, optimizer, device, n_epochs=10, val_s
     for epoch in range(n_epochs):
         model.train()
         running_loss = 0.0
-        for inputs, labels in tqdm(train_loader, desc=f'Training Epoch {epoch + 1}/{n_epochs}'):
+        for data in tqdm(train_loader, desc=f'Training Epoch {epoch + 1}/{n_epochs}'):
+            inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device).float()
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -47,17 +48,20 @@ def train_model(model, dataset, criterion, optimizer, device, n_epochs=10, val_s
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
+            
         avg_train_loss = running_loss / len(train_loader)
         train_losses.append(avg_train_loss)
 
         model.eval()
         running_loss = 0.0
         with torch.no_grad():
-            for inputs, labels in val_loader:
+            for data in val_loader:
+                inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device).float()
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 running_loss += loss.item()
+                
         avg_val_loss = running_loss / len(val_loader)
         val_losses.append(avg_val_loss)
 
@@ -91,24 +95,22 @@ def test_model(model, test_data, device):
     test_loader = DataLoader(test_data, batch_size=64, shuffle=False, drop_last=False)
     model.eval()
     model.to(device)
-    correct = 0
-    total = 0
     all_labels = []
-    all_preds = []
+    all_predictions = []
+    
     with torch.no_grad():
-        for images, labels in tqdm(test_loader, desc="Testing"):
+        for data in tqdm(test_loader, desc="Testing"):
+            images, labels = data
             images, labels = images.to(device), labels.to(device).float()
             outputs = model(images)
             predicted = torch.round(outputs)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
             all_labels.extend(labels.cpu().numpy())
-            all_preds.extend(predicted.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
 
-    accuracy = 100 * correct / total
-    precision = precision_score(all_labels, all_preds)
-    recall = recall_score(all_labels, all_preds)
-    f1 = f1_score(all_labels, all_preds)
+    accuracy = accuracy_score(all_labels, all_predictions)
+    precision = precision_score(all_labels, all_predictions, average='binary')
+    recall = recall_score(all_labels, all_predictions, average='binary')
+    f1 = f1_score(all_labels, all_predictions, average='binary')
 
     return accuracy, precision, recall, f1
 
